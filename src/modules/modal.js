b1148da9e15b9df79a618177930b13abbebe9b27
NastyaSaprick <sapronovaas2002@gmail.com>
2022-01-27 17:09:29 +0300

    модальное окно

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -12,6 +12,16 @@ const modal = () => {
   closeBtn.addEventListener('click', () => {
     modal.style.display = 'none'
   })
+
+  //при нажатии мимо модального окна - оно закрывается
+  modal.addEventListener('click', (e) => {
+    if (
+      !e.target.closest(".popup-content") ||
+      e.target.classList.contains("popup-close")
+    ) {
+      modal.style.display = "none";
+    }
+  })
 }
 
 export default modal
