4c4692835496e25fb0b444e6402db8f359dccf15
NastyaSaprick <sapronovaas2002@gmail.com>
2022-02-01 18:23:33 +0300

    калькулятор

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import modal  from "./modules/modal";
 import validator  from "./modules/validator";
 import tabs  from "./modules/tabs";
 import slider  from "./modules/slider";
+import calc  from "./modules/calc";
 
 timer('28 january 2022')
 menu()
@@ -12,5 +13,6 @@ modal()
 validator()
 tabs()
 slider()
+calc(100)
 
 //npm run watch
