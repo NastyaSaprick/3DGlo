caac93d7c044261166c13bfef9f49d96d78eda80
NastyaSaprick <sapronovaas2002@gmail.com>
2022-02-07 13:28:36 +0300

    отправка форм

--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@
 import timer  from "./modules/timer";
 import menu  from "./modules/menu";
 import modal  from "./modules/modal";
-import validator  from "./modules/validator";
+// import validator  from "./modules/validator";
 import tabs  from "./modules/tabs";
 import slider  from "./modules/slider";
 import calc  from "./modules/calc";
+import sendForm  from "./modules/sendForm";
 
 timer('28 january 2022')
 menu()
 modal()
-validator()
+// validator()
 tabs()
 slider()
 calc(100)
+sendForm({
+  formId: 'form1',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total'
+    }
+  ]
+})
 
 //npm run watch
